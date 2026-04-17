adb4bcdf66044f4ee5bc561ef0e85e025b6e7f07
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-17 16:49:51 -0400

    Use legacy tailwind.config.js file for font and color

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,14 +9,8 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`${montserrat.variable} font-mont`}>
-        <Component {...pageProps} />
-      </main>
-    </>
+    <main className={ `${montserrat.variable} font-mont` }>
+      <Component {...pageProps} />
+    </main>
   );
 }
