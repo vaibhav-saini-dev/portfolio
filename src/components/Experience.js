10f582bee17cab48f1befbf0e5c87102a15cc54a
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-19 19:33:59 -0400

    Initialize scroll line on Experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { useScroll } from 'framer-motion'
+import React, { useRef } from 'react'
+import { motion, useScroll } from 'framer-motion'
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
-  return <li className="my-8 first:mt-0 last:mb-0 w-[66] mx-auto flex flex-col items-center justify-between">
+  return <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
     <div>
-      <h3 className="capitalize font-bold text-2xl">{position}&nbsp;<a href={companyLink}
-        target="_blank"
-        className="text-primary capitalize"
-      >@{company}</a></h3>
+      <h3 className="capitalize font-bold text-2xl">
+        {position}&nbsp;
+        <a
+          href={companyLink}
+          target="_blank"
+          className="text-primary capitalize"
+        >
+          @{company}
+        </a>
+      </h3>
       <span className="capitalize font-medium text-dark/75">
         {time} | {address}
       </span>
@@ -18,22 +24,32 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
   </li>
 }
 const Experience = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll(
+    {
+      target: ref,
+      offset: ["start end", "end start"]
+    }
+  )
+
   return (
-    <div className="m-64">
-      <h2 className="font-bold text-8xl mb-32 2-full text-center">
+    <div className="my-64">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center">
         Experience
       </h2>
 
-      <div className="w-[75] mx-auto relative">
-        <div className="absolute left-8 top-0 w-1 h-full bg-dark origin-top" />
+      <div ref={ref} className="w-[75%] mx-auto relative">
+        <motion.div
+          style={{scaleY: scrollYProgress}}
+          className="absolute left-8 top-0 w-1 h-full bg-dark origin-top" />
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
             position="Software Engineer" company="Google"
             companyLink="www.google.com"
             time="2022-Present" address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's
-          search engine, including improving the accuracy and relevance of search results and
-          developing new tools for data analysis and visualization."
+            search engine, including improving the accuracy and relevance of search results and
+            developing new tools for data analysis and visualization."
           />
 
           <Details
@@ -41,8 +57,8 @@ const Experience = () => {
             companyLink="www.google.com"
             time="2022-Present" address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's
-          search engine, including improving the accuracy and relevance of search results and
-          developing new tools for data analysis and visualization."
+            search engine, including improving the accuracy and relevance of search results and
+            developing new tools for data analysis and visualization."
           />
 
           <Details
@@ -50,8 +66,8 @@ const Experience = () => {
             companyLink="www.google.com"
             time="2022-Present" address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's
-          search engine, including improving the accuracy and relevance of search results and
-          developing new tools for data analysis and visualization."
+            search engine, including improving the accuracy and relevance of search results and
+            developing new tools for data analysis and visualization."
           />
 
           <Details
@@ -59,8 +75,8 @@ const Experience = () => {
             companyLink="www.google.com"
             time="2022-Present" address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's
-          search engine, including improving the accuracy and relevance of search results and
-          developing new tools for data analysis and visualization."
+            search engine, including improving the accuracy and relevance of search results and
+            developing new tools for data analysis and visualization."
           />
 
           <Details
@@ -68,17 +84,17 @@ const Experience = () => {
             companyLink="www.google.com"
             time="2022-Present" address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's
-          search engine, including improving the accuracy and relevance of search results and
-          developing new tools for data analysis and visualization."
+            search engine, including improving the accuracy and relevance of search results and
+            developing new tools for data analysis and visualization."
           />
 
           <Details
-          position="Software Engineer" company="Google"
-          companyLink="www.google.com"
-          time="2022-Present" address="Mountain View, CA"
-          work="Worked on a team responsible for developing new features for Google's
-          search engine, including improving the accuracy and relevance of search results and
-          developing new tools for data analysis and visualization."
+            position="Software Engineer" company="Google"
+            companyLink="www.google.com"
+            time="2022-Present" address="Mountain View, CA"
+            work="Worked on a team responsible for developing new features for Google's
+            search engine, including improving the accuracy and relevance of search results and
+            developing new tools for data analysis and visualization."
           />
         </ul>
       </div>
