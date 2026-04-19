927af2e70f17ea3dd0e93151595e91fb0e2b2ba2
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-19 10:46:47 -0400

    Add Resume and Contact Button on Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import profilePic from "../../public/images/profile/Frieren_Transparent.png"
 // import profilePic from "../../public/images/profile/developer-pic-1.png"
 import Image from 'next/image'
 import AnimatedText from '@/components/AnimatedText'
+import Link from 'next/link'
+import { LinkArrow } from '@/components/Icons'
 
 export default function Home() {
   return (
@@ -22,10 +24,24 @@ export default function Home() {
             {/* ! overrides classes. 
                 Example: !text-6xl overrides text-8xl in AnimatedText.js */}
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className="!text-6xl !text-left"/>
-              <p className="mt-4 text-lg">
+              <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link href="/dummy.pdf" target={"_blank"}
+                className="flex items-center bg-dark text-light p-2.5 px-6
+                rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
+                border-2 border-solid border-transparent hover:border-dark
+                "
+                download={true}
+                >Resume <LinkArrow className={"w-6 ml-1"}/>
+                
+                </Link>
+                <Link href="mailto:vaibhav.saini.professional@proton.me" target={"_blank"}
+                className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >Contact</Link>
+              </div>
             </div>
           </div>
         </Layout>
