d47c0ad68750b7065177e3c59730241ce382ee3a
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-19 19:50:51 -0400

    Update scroll animation on Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,34 +1,47 @@
 import React, { useRef } from 'react'
 import { motion, useScroll } from 'framer-motion'
+import LiIcon from './LiIcon';
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
-  return <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
-    <div>
-      <h3 className="capitalize font-bold text-2xl">
-        {position}&nbsp;
-        <a
-          href={companyLink}
-          target="_blank"
-          className="text-primary capitalize"
-        >
-          @{company}
-        </a>
-      </h3>
-      <span className="capitalize font-medium text-dark/75">
-        {time} | {address}
-      </span>
-      <p className="font-medium w-full">
-        {work}
-      </p>
-    </div>
-  </li>
-}
+  const ref = useRef(null);
+
+  return (
+    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+      
+      <LiIcon reference={ref}/>
+      
+      <motion.div
+      initial={{y:50}}
+      whileInView={{y:0}}
+      transition={{duration:0.5, type:"spring"}}
+      >
+        <h3 className="capitalize font-bold text-2xl">
+          {position}&nbsp;
+          <a
+            href={companyLink}
+            target="_blank"
+            className="text-primary capitalize"
+          >
+            @{company}
+          </a>
+        </h3>
+        <span className="capitalize font-medium text-dark/75">
+          {time} | {address}
+        </span>
+        <p className="font-medium w-full">
+          {work}
+        </p>
+      </motion.div>
+    </li>
+  );
+};
+
 const Experience = () => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll(
     {
       target: ref,
-      offset: ["start end", "end start"]
+      offset: ["start end", "center start"]
     }
   )
 
@@ -40,8 +53,8 @@ const Experience = () => {
 
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
-          style={{scaleY: scrollYProgress}}
-          className="absolute left-8 top-0 w-1 h-full bg-dark origin-top" />
+          style={{ scaleY: scrollYProgress }}
+          className="absolute left-9 top-0 w-1 h-full bg-dark origin-top" />
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
             position="Software Engineer" company="Google"
