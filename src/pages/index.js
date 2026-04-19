506c0cae12775c8541987d6a94223f0d70421ee1
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-19 12:23:16 -0400

    Add Hire Me Button and Lightbulb
    Hire Me button links to my work email.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ import Image from 'next/image'
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
-
+import HireMe from '@/components/HireMe'
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg"
 export default function Home() {
   return (
     <>
@@ -45,6 +46,11 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+
+        <HireMe />
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image src={lightBulb} alt="Cartoon Yellow Lightbulb" className="w-full h-auto" />
+        </div>
       </main>
     </>
   )
