a67a8fa3b7ead38c73304ae93cc6fdbdb9a2141e
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-20 17:12:44 -0400

    Update Experience & Education Design on dark mode

--- src/components/Education.js
+++ src/components/Education.js
@@ -20,7 +20,7 @@ const Details = ({ type, time, place, info }) => {
         <h3 className="capitalize font-bold text-2xl">
           {type}
         </h3>
-        <span className="capitalize font-medium text-dark/75">
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
           {time} | {place}
         </span>
         <p className="font-medium w-full">
@@ -53,7 +53,7 @@ const Education = () => {
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-1 h-full bg-dark origin-top" />
+          className="absolute left-9 top-0 w-1 h-full bg-dark origin-top dark:bg-light" />
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
             type="Bachelor Of Science In Computer Science" 
