1190e6a43ad256526c72ba7e69b265f1765e1126
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-21 11:42:05 -0400

    Make Project page responsive

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -19,14 +19,15 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
     return (
         <article className="w-full flex items-center justify-between relative rounded-br-2xl
         rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light
+        lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4
         ">
             <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light
-            rounded-br-3xl
+            rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]
             " />
             <Link
                 href={link}
                 target="_blank"
-                className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+                className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
             >
                 <FramerImage src={img} alt={title} className="w-full h-auto" 
                 whileHover={{scale:1.05}}
@@ -38,17 +39,18 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
                 />
             </Link>
 
-            <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-                <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
+            <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+                <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">{type}</span>
                 <Link href={link} target="_blank" className="hover:underline underline-offset-2">
-                    <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light">{title}</h2>
+                    <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">{title}</h2>
                 </Link>
-                <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
+                <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">{summary}</p>
                 <div className="mt-2 flex items-center">
                     <Link href={github} target="_blank" className="w-10"> <GitHubIcon /> </Link>
                     <Link href={link} target="_blank"
                         className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
                         dark:bg-light dark:text-dark
+                        sm:px-4 sm:text-base
                         "
                     > Visit Project </Link>
                 </div>
@@ -60,10 +62,10 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
 const Project = ({ title, type, img, link, github }) => {
     return (
         <article className="w-full flex flex-col items-center justify-center rounded-2xl
-        border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light
+        border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4
         ">
             <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-4xl bg-dark 
-            rounded-br-3xl dark:bg-light
+            rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]
             " />
             <Link
                 href={link}
@@ -77,16 +79,16 @@ const Project = ({ title, type, img, link, github }) => {
             </Link>
 
             <div className="w-full flex flex-col items-start justify-between mt-4">
-                <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
+                <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">{type}</span>
                 <Link href={link} target="_blank" className="hover:underline underline-offset-2">
-                    <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+                    <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">{title}</h2>
                 </Link>
                 <div className="w-full mt-2 flex items-center justify-between">
                     <Link href={link} target="_blank"
-                        className="text-lg font-semibold underline"
+                        className="text-lg font-semibold underline md:text-base"
                     > Visit
                     </Link>
-                    <Link href={github} target="_blank" className="w-8"> <GitHubIcon /> </Link>
+                    <Link href={github} target="_blank" className="w-8 md:w-6"> <GitHubIcon /> </Link>
                 </div>
             </div>
         </article>
@@ -103,10 +105,10 @@ const projects = () => {
             <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
                 <Layout className="pt-16">
                     <AnimatedText text="Built with Purpose"
-                        className="mb-16"
+                        className="mb-16 lg:text-7xl! sm:mb-8 sm:text-6xl! xs:text-4xl!"
                     />
                     {/* gap-24 is the gap between x and y */}
-                    <div className="grid grid-cols-12 gap-24">
+                    <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
                         <div className="col-span-12">
                             <FeaturedProject
                                 type="PWA + Vite React + API"
@@ -119,7 +121,7 @@ Uses live data from the popular video game Fortnite, showcasing current maps and
                                 github="https://github.com/vaibhav-saini-dev/FortniteInfo"
                             />
                         </div>
-                        <div className="col-span-6">
+                        <div className="col-span-6 sm:col-span-12">
                             <Project
                                 type="Low-level programming"
                                 title="Bash in C"
@@ -128,7 +130,7 @@ Uses live data from the popular video game Fortnite, showcasing current maps and
                                 github="https://github.com/vaibhav-saini-dev/bash-in-C"
                             />
                         </div>
-                        <div className="col-span-6">
+                        <div className="col-span-6 sm:col-span-12">
                             <Project
                                 type="Unity 2D"
                                 title="Shotgun Skrimsli"
@@ -148,7 +150,7 @@ Uses live data from the popular video game Fortnite, showcasing current maps and
                                 github="https://github.com/vaibhav-saini-dev/Smart-Classroom-Attendance-System"
                             />
                         </div>
-                        <div className="col-span-6">
+                        <div className="col-span-6 sm:col-span-12">
                             <Project
                                 type="Website & API"
                                 title="Analyzing Boston Crime Data"
@@ -157,7 +159,7 @@ Uses live data from the popular video game Fortnite, showcasing current maps and
                                 github="https://github.com/vaibhav-saini-dev/CS480-HW3-BONUS"
                             />
                         </div>
-                        <div className="col-span-6">
+                        <div className="col-span-6 sm:col-span-12">
                             <Project
                                 type="Featured Project"
                                 title="Crypto Screener Application"
