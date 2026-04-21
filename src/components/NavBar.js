63d879d678cc8448036e2d10b6c51583c0c9569d
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-21 10:00:44 -0400

    Create mobile navbar menu
    Shows a mobile navbar menu when the hamburger icon is clicked

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,8 +21,32 @@ const CustomLink = ({ href, title, className = "" }) => {
             dark:bg-light`}
             >&nbsp;</span>
         </Link>
-    )
-}
+    );
+};
+
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        toggle();
+        router.push(href)
+    }
+
+    return (
+        <button href={href} className={`${className} relative group text-light dark:text-dark my-2`} onClick={handleClick}>
+            {title}
+
+            <span className={`
+            h-[1px] inline-block bg-light
+            absolute left-0 -bottom-0.5
+            group-hover:w-full transition-[width] ease duration-300
+            ${router.asPath === href ? 'w-full' : 'w-0'}
+            dark:bg-dark`}
+            >&nbsp;</span>
+        </button>
+    );
+};
+
 const NavBar = () => {
 
     const [mode, setMode] = useThemeSwitcher();
@@ -38,7 +62,7 @@ const NavBar = () => {
         //               justify: for horizontal alignment
         <header
             className='w-full px-32 py-8 font-medium flex items-center justify-between
-            dark:text-light
+            dark:text-light relative
             '
         >
             <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
@@ -99,7 +123,64 @@ const NavBar = () => {
                 </nav>
             </div>
 
+            {
+                isOpen ?
+                    <div className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+                        bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32
+                        ">
+                        <nav className="flex items-center flex-col justify-center">
+                            <CustomMobileLink href="/" title="Home" className="" toggle={handleClick} />
+                            <CustomMobileLink href="/about" title="About" className="" toggle={handleClick} />
+                            <CustomMobileLink href="/projects" title="Projects" className="" toggle={handleClick} />
+                            {/* <CustomLink href="/articles" title="Experience" className="mx-4" /> */}
+                            {/* <CustomLink href="/articles" title="Skills" className="ml-4" /> */}
+                        </nav>
 
+                        <nav className="flex items-center justify-center flex-wrap mt-2">
+                            <motion.a href="https://www.linkedin.com/in/vaibhav-saini-coder/" target={"_blank"}
+                                whileHover={{ y: -2 }}
+                                whileTap={{ scale: 0.9 }}
+                                className="w-6 mr-3 sm:mx-1"
+                            >
+                                <LinkedInIcon />
+                            </motion.a>
+                            <motion.a href="https://github.com/vaibhav-saini-dev" target={"_blank"}
+                                whileHover={{ y: -2 }}
+                                whileTap={{ scale: 0.9 }}
+                                className="w-6 mx-3 bg-light rounded-full dark:bg-dark sm:mx-1"
+                            >
+                                <GitHubIcon />
+                            </motion.a>
+                            <motion.a href="https://leetcode.com/u/vaibhavsaini294/" target={"_blank"}
+                                whileHover={{ y: -2 }}
+                                whileTap={{ scale: 0.9 }}
+                                className="w-6 mx-3 bg-light rounded-full dark:bg-transparent sm:mx-1"
+                            >
+                                <LeetCodeIcon />
+                            </motion.a>
+                            <motion.a href="mailto:vaibhav.saini.professional@proton.me" target={"_blank"}
+                                whileHover={{ y: -2 }}
+                                whileTap={{ scale: 0.9 }}
+                                className="w-6 ml-3 bg-light border-light border-[0.1rem] dark:bg-light dark:border-0 sm:mx-1"
+                            >
+                                <MailIcon />
+                            </motion.a>
+
+                            <button
+                                onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                                className={`ml-3 flex items-center justify-center rounded-full p-1
+                                    ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
+                            >
+                                {
+                                    mode === "dark" ?
+                                        <SunIcon className={"fill-dark"} />
+                                        : <MoonIcon className={"fill-dark"} />
+                                }
+                            </button>
+                        </nav>
+                    </div>
+                : null
+            }
             <div className="absolute left-[50%] top-2 translate-x-[-50%]">
                 <Logo />
             </div>
