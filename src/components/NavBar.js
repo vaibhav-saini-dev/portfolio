f44a1a85472bd0903b1f219eb3471c225d3fc8ec
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-21 10:28:20 -0400

    Make Home page responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -62,7 +62,7 @@ const NavBar = () => {
         //               justify: for horizontal alignment
         <header
             className='w-full px-32 py-8 font-medium flex items-center justify-between
-            dark:text-light relative
+            dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8
             '
         >
             <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
