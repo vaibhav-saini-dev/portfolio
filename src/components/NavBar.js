cca037234dca1391cd9675dc3bca202bea4c9c14
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-21 09:36:13 -0400

    Hide navbar at lg custom breakpoint

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -42,60 +42,63 @@ const NavBar = () => {
             '
         >
             <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
-                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5' }`}></span>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
                 <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
-                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5' }`}></span>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
             </button>
-            <nav>
-                <CustomLink href="/" title="Home" className="mr-4" />
-                <CustomLink href="/about" title="About" className="mx-4" />
-                <CustomLink href="/projects" title="Projects" className="mx-4" />
-                {/* <CustomLink href="/articles" title="Experience" className="mx-4" /> */}
-                {/* <CustomLink href="/articles" title="Skills" className="ml-4" /> */}
-            </nav>
+            <div className="w-full flex justify-between items-center lg:hidden">
+                <nav>
+                    <CustomLink href="/" title="Home" className="mr-4" />
+                    <CustomLink href="/about" title="About" className="mx-4" />
+                    <CustomLink href="/projects" title="Projects" className="mx-4" />
+                    {/* <CustomLink href="/articles" title="Experience" className="mx-4" /> */}
+                    {/* <CustomLink href="/articles" title="Skills" className="ml-4" /> */}
+                </nav>
 
-            <nav className="flex items-center justify-center flex-wrap">
-                <motion.a href="https://www.linkedin.com/in/vaibhav-saini-coder/" target={"_blank"}
-                    whileHover={{ y: -2 }}
-                    whileTap={{scale:0.9}}
-                    className="w-6 mr-3"
-                >
-                    <LinkedInIcon />
-                </motion.a>
-                <motion.a href="https://github.com/vaibhav-saini-dev" target={"_blank"}
-                    whileHover={{ y: -2 }}
-                    whileTap={{scale:0.9}}
-                    className="w-6 mx-3"
-                >
-                    <GitHubIcon />
-                </motion.a>
-                <motion.a href="https://leetcode.com/u/vaibhavsaini294/" target={"_blank"}
-                    whileHover={{ y: -2 }}
-                    whileTap={{scale:0.9}}
-                    className="w-6 mx-3 dark:bg-light dark:rounded-full"
-                >
-                    <LeetCodeIcon />
-                </motion.a>
-                <motion.a href="mailto:vaibhav.saini.professional@proton.me" target={"_blank"}
-                    whileHover={{ y: -2 }}
-                    whileTap={{scale:0.9}}
-                    className="w-6 ml-3 dark:bg-light dark:border-[0.1rem] dark:border-solid dark:border-light"
-                >
-                    <MailIcon />
-                </motion.a>
-            
-            <button
-            onClick={() => setMode(mode === "light" ? "dark" : "light")}
-            className={`ml-3 flex items-center justify-center rounded-full p-1
+                <nav className="flex items-center justify-center flex-wrap">
+                    <motion.a href="https://www.linkedin.com/in/vaibhav-saini-coder/" target={"_blank"}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className="w-6 mr-3"
+                    >
+                        <LinkedInIcon />
+                    </motion.a>
+                    <motion.a href="https://github.com/vaibhav-saini-dev" target={"_blank"}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className="w-6 mx-3"
+                    >
+                        <GitHubIcon />
+                    </motion.a>
+                    <motion.a href="https://leetcode.com/u/vaibhavsaini294/" target={"_blank"}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className="w-6 mx-3 dark:bg-light dark:rounded-full"
+                    >
+                        <LeetCodeIcon />
+                    </motion.a>
+                    <motion.a href="mailto:vaibhav.saini.professional@proton.me" target={"_blank"}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className="w-6 ml-3 dark:bg-light dark:border-[0.1rem] dark:border-solid dark:border-light"
+                    >
+                        <MailIcon />
+                    </motion.a>
+
+                    <button
+                        onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                        className={`ml-3 flex items-center justify-center rounded-full p-1
                 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
-            >
-                {
-                    mode === "dark" ?
-                    <SunIcon className={"fill-dark"} />
-                    : <MoonIcon className={"fill-dark"} />
-                }
-            </button>
-            </nav>
+                    >
+                        {
+                            mode === "dark" ?
+                                <SunIcon className={"fill-dark"} />
+                                : <MoonIcon className={"fill-dark"} />
+                        }
+                    </button>
+                </nav>
+            </div>
+
 
             <div className="absolute left-[50%] top-2 translate-x-[-50%]">
                 <Logo />
