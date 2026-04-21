8c091bbcb466abde4cf9cd7f0d49d4e313b888a7
vaibhav-saini-dev <vaibhav.saini.professional@proton.me>
2026-04-21 09:34:21 -0400

    Display hamburger icon on lg custom breakpoint

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,7 +41,7 @@ const NavBar = () => {
             dark:text-light
             '
         >
-            <button className="flex flex-col justify-center items-center" onClick={handleClick}>
+            <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
                 <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5' }`}></span>
                 <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
                 <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5' }`}></span>
